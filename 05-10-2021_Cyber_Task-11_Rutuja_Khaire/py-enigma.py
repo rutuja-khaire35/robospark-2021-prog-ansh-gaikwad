a3a67a2a7df1441628688cf0b4f3cdec0aa4eb16
rutuja-khaire35 <82515776+rutuja-khaire35@users.noreply.github.com>
2021-10-07 23:32:21 +0530

    Task-11

---
+++ 05-10-2021_Cyber_Task-11_Rutuja_Khaire/py-enigma.py
@@ -0,0 +1,31 @@
+import sys
+from enigma.machine import EnigmaMachine
+
+def brute_ring_settings(ciphertext):
+    for a in range(1, 26):
+        for b in range(1, 26):
+            for c in range(1, 26):
+                machine = EnigmaMachine.from_key_sheet(
+                   rotors='I II III',
+                   reflector='B',
+                   ring_settings=[a, b, c],
+                   plugboard_settings='AV BS CG DL FU HZ IN KM OW RX')
+                plaintext = machine.process_text(ciphertext)
+                if plaintext.startswith('THISIS'):
+                    print('[+] Ring settings: {}, {}, {}'.format(a, b, c))
+                    return a, b, c
+    return -1, -1, -1
+
+a, b, c = brute_ring_settings('FKYCEPWYRXTXDHYGUPNG')
+
+if a < 0 or b < 0 or c < 0:
+    print('[-] Failed')
+    sys.exit(0)
+
+machine = EnigmaMachine.from_key_sheet(
+   rotors='I II III',
+   reflector='B',
+   ring_settings=[a, b, c],
+   plugboard_settings='AV BS CG DL FU HZ IN KM OW RX')
+plaintext = machine.process_text('DPIWPOLLFEUPIKAKLQLC')
+print('[+] Flag: ' + plaintext)
